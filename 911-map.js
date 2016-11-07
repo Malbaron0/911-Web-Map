518648d9c9c9b3147ccc37025235d2523b920e75
Malyar Baron <malbaron0@gmail.com>
2016-11-06 21:43:11 -0500

    Added InfoWindow to each Marker
    Each marker will now have an appropriate infoWindow.
    
    Also map centers on the specified borough user chooses.

--- 911-map.js
+++ 911-map.js
@@ -4,24 +4,87 @@ function initialize(jsonParsed) {
 
     var mapCanvas = document.getElementById('googleMap');
     var nycOpenData = jsonParsed;
-    var myCenter = new google.maps.LatLng(40.5989055802915, -74.00132831970851);
+    var myCenter;
+
+    //Depending on what use chooses as a borough, the map will focus on that location when user searches.
+    switch(offenseSearchDetail.offenseBorough){
+      case "BROOKLYN":
+      myCenter = new google.maps.LatLng(40.650002,-73.949997);
+      break;
+      case "QUEENS":
+      myCenter = new google.maps.LatLng(40.742054,-73.769417);
+      break;
+      case "BRONX":
+      myCenter = new google.maps.LatLng(40.837048,-73.865433);
+      break;
+      case "STATEN ISLAND":
+      myCenter = new google.maps.LatLng(40.579021,-74.151535);
+      break;
+      case "MANHATTAN":
+      myCenter = new google.maps.LatLng(40.758896,-73.985130);
+      break;
+      default:
+      myCenter = new google.maps.LatLng(40.650002,-73.949997);
+    }
+
+
     var mapProp = {
-        center: myCenter, // needs LatLng, using myCenter for now.
+        center: myCenter,
         zoom: 12,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
+
+
+    var infowindow = new google.maps.InfoWindow(); //only need to make infowindow object once, and use in loop
     var map = new google.maps.Map(mapCanvas, mapProp); // map object
 
 
-    for (var value in nycOpenData) {
+    //function adds an event listener to each marker. when clicked an infowindow is shown with some information about that specific marker.
+    function markTheMap(nycOpenDataObject){
+      var latLng = new google.maps.LatLng(nycOpenDataObject.location_1.coordinates[1], nycOpenDataObject.location_1.coordinates[0]);
+
+      var marker = new google.maps.Marker({
+          position: latLng,
+          map: map
+      });
+
+        google.maps.event.addListener(marker, 'click', function(){
+          infowindow.close(); // Close previously opened infowindow
+          infowindow.setContent(googleMapInfoWindow.call(nycOpenDataObject));
+                infowindow.maxWidth = 50;
+                infowindow.open(map, marker);
+        });
+
+    }
+
+    //go through each value in the json and mark it on the map and give appropriate infowindow.
+    for (var value in nycOpenData){
+      markTheMap(nycOpenData[value]);
+    }
+
+
+
+
+
+
+    // closure problem with this method. refer to http://stackoverflow.com/a/30479554
+    /*for (var value in nycOpenData) {
         var marker = new google.maps.Marker({
             position: new google.maps.LatLng(nycOpenData[value].location_1.coordinates[1], nycOpenData[value].location_1.coordinates[0]),
             title: 'map911'
+            map: map
         });
-        //console.log(nycOpenData[value].location_1.coordinates[1]+'  ' +nycOpenData[value].location_1.coordinates[0])
-        marker.setMap(map);
 
-    }
+          google.maps.event.addListener(marker, 'click', function(){
+            infowindow.close(); // Close previously opened infowindow
+            infowindow.setContent( "<div id='infowindow'>"+ nycOpenDataObject.precinct +"</div>");
+            infowindow.open(map, marker);
+          });
+
+        //console.log(nycOpenData[value].location_1.coordinates[1]+'  ' +nycOpenData[value].location_1.coordinates[0])
+        //marker.setMap(map);
+
+    }*/
 
     // tool tip on top of the marker
     /*
@@ -33,12 +96,28 @@ function initialize(jsonParsed) {
 }
 
 
-//AJAX request to
+//HTML for the info window when clicked on the marker.
+function googleMapInfoWindow(info){
+  var parsedDate = moment(this.occurrence_date).format("MMMM Do YYYY, h:mm: a");
+
+  var contentString = '<div class="info-window">' +
+        '<h3>'+this.borough+'</h3>' +
+        '<div class="info-content">' +
+        '<p><b>Offense:\t</b>'+this.offense+'</p>' +
+        '<p><b>Occurence Date:\t</b>'+parsedDate+'</p>' +
+        '<p><b>Day:\t</b>'+this.day_of_week+'</p>' +
+        '<p><b>Precinct:\t</b>'+this.precinct+'</p>' +
+        '<p><b>Jurisdiction:\t</b>'+this.jurisdiction+'</p>'+
+        '</div>' +
+        '</div>';
+  return contentString;
+}
+
+//AJAX request to cityofnewyork API. return the response parsed as JSON
 function startRequest(url) {
     var httpRequest;
-    //Added multiple queries--- TODO: Make navigation bar for user to input these queries to make easy to read.
     //var url = 'https://data.cityofnewyork.us/resource/e4qk-cpnv.json?occurrence_year=2006&occurrence_month=Sep&offense=ROBBERY&borough=BROOKLYN';
-    var testData;
+    var dataSet;
     console.log(url);
     httpRequest = new XMLHttpRequest();
 
@@ -53,21 +132,27 @@ function startRequest(url) {
 
 
     function NYCOpenDataResponse() {
-        if (httpRequest.readyState === XMLHttpRequest.DONE) {
-            if (httpRequest.status === 200) {
-                testData = httpRequest.responseText;
-                return testData;
-            } else {
+      try {
+          if (httpRequest.readyState === XMLHttpRequest.DONE) {
+              if (httpRequest.status === 200) {
+                  dataSet = httpRequest.responseText;
+                  //return testData;
+              }
+              else {
                 alert('There was a problem with the request.');
-            }
-        }
+              }
+          }
     }
+    catch (e) {
+        alert('Problem with server');
+        console.log(e.description);
+    }
+  }
 
-    var JSONobject = JSON.parse(testData);
-    return JSONobject;
+    return parsingToJSON(dataSet);
 }
 
-function parsingJSONRequest(text) {
+function parsingToJSON(text) {
     var JSONobject = JSON.parse(text);
     return JSONobject;
 }
