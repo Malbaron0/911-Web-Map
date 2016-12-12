bcd552835bacec82f892b5a158ba4a61bef94924
Malyar Baron <malbaron0@gmail.com>
2016-12-11 23:37:22 -0500

    Added Year, Added infobubble and refactoring
    Added the year option in the search options. User can either choose all years from 2015 or less or
    2016. if using 2015 and less, another api is called.
    
    Infobubble library is being used here instead of the default google map infowindow

--- 911-map.js
+++ 911-map.js
@@ -32,20 +32,14 @@ function initialize(jsonParsed) {
         zoom: 12
     };
 
-    //black and white style for google maps https://snazzymaps.com/style/8097/wy
-    var styledMapType = new google.maps.StyledMapType(
-      [
-        {"featureType":"all","elementType":"geometry.fill","stylers":[{"weight":"2.00"}]},
-        {"featureType":"all","elementType":"geometry.stroke","stylers":[{"color":"#9c9c9c"}]},{"featureType":"all","elementType":"labels.text","stylers":[{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},
-        {"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},
-        {"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#eeeeee"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":
-        [{"color":"#7b7b7b"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon",
-        "stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},
-        {"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#c8d7d4"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#070707"}]},
-        {"featureType":"water","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]}] , {name: 'Styled Map'});
+    var styledMapType = mapStyle();
+    //var infowindow = new google.maps.InfoWindow(); //only need to make infowindow object once, and use in loop
+    infoBubble = new InfoBubble({
+          maxWidth: 300,
+          backgroundColor: '#428BCA'
+        });
+    infoBubble.tabsContainer_.style['display'] = 'none'; //remove the tab feature of infobubble.
 
-
-    var infowindow = new google.maps.InfoWindow(); //only need to make infowindow object once, and use in loop
     var map = new google.maps.Map(mapCanvas, mapProp); // map object
 
     //set the style type
@@ -61,7 +55,7 @@ function initialize(jsonParsed) {
 
         //Mark the object on the map if it has the same offense month as what the user chose.
         if (parsedDateMonth == offenseSearchDetail.offenseMonth) {
-          console.log(parsedDateMonth);
+            console.log(parsedDateMonth);
 
             var latLng = new google.maps.LatLng(nycOpenDataObject.latitude, nycOpenDataObject.longitude);
             var marker = new google.maps.Marker({
@@ -70,13 +64,16 @@ function initialize(jsonParsed) {
             });
 
             google.maps.event.addListener(marker, 'click', function() {
-                infowindow.close(); // Close previously opened infowindow
-                infowindow.setContent(googleMapInfoWindow.call(nycOpenDataObject));
-                infowindow.maxWidth = 50;
-                infowindow.open(map, marker);
+
+                infoBubble.close(); // Close previously opened infowindow
+
+                infoBubble.addTab(nycOpenDataObject.boro_nm, googleMapInfoWindow.call(nycOpenDataObject));
+                infoBubble.open(map, marker);
+                //infowindow.setContent(googleMapInfoWindow.call(nycOpenDataObject));
+                //infowindow.maxWidth = 250;
+                //infowindow.open(map, marker);
             });
-        }
-        else {
+        } else {
             console.log('Month not avaliable');
             return;
         }
@@ -116,20 +113,20 @@ function initialize(jsonParsed) {
 }
 
 //HTML for the info window when clicked on the marker.
-function googleMapInfoWindow(){
-  var parsedDate = moment(this.rpt_dt).format("MMMM Do YYYY");  //use this to get the yaer and let user search by year.
+function googleMapInfoWindow() {
+    var parsedDate = moment(this.rpt_dt).format("MMMM Do YYYY"); //use this to get the yaer and let user search by year.
 
-  var contentString = '<div class="info-window">' +
-        '<h3>'+this.boro_nm+'</h3>' +
+    var contentString = '<div class="iw-content">' +
+        '<h3>' + this.boro_nm + '</h3>' +
         '<div class="info-content">' +
-        '<p><b>Offense:\t</b>'+this.ofns_desc+'</p>' +
-        '<p><b>Offense Description:\t</b>'+this.pd_desc+'</p>' +
-        '<p><b>Location Type:\t</b>'+this.prem_typ_desc+'</p>' +
-        '<p><b>Occurence Date:\t</b>'+parsedDate+'</p>'+
-        '<p><b>Complaint Number:\t</b>'+this.cmplnt_num+'</p>' +
+        '<p><b>Offense:\t</b>' + this.ofns_desc + '</p>' +
+        '<p><b>Offense Description:\t</b>' + this.pd_desc + '</p>' +
+        '<p><b>Location Type:\t</b>' + this.prem_typ_desc + '</p>' +
+        '<p><b>Occurence Date:\t</b>' + parsedDate + '</p>' +
+        '<p><b>Complaint Number:\t</b>' + this.cmplnt_num + '</p>' +
         '</div>' +
         '</div>';
-  return contentString;
+    return contentString;
 }
 
 //AJAX request to cityofnewyork API. return the response parsed as JSON
@@ -151,22 +148,20 @@ function startRequest(url) {
 
 
     function NYCOpenDataResponse() {
-      try {
-          if (httpRequest.readyState === XMLHttpRequest.DONE) {
-              if (httpRequest.status === 200) {
-                  dataSet = httpRequest.responseText;
-                  //return testData;
-              }
-              else {
-                alert('There was a problem with the request.');
-              }
-          }
+        try {
+            if (httpRequest.readyState === XMLHttpRequest.DONE) {
+                if (httpRequest.status === 200) {
+                    dataSet = httpRequest.responseText;
+                    //return testData;
+                } else {
+                    alert('There was a problem with the request.');
+                }
+            }
+        } catch (e) {
+            alert('Problem with server');
+            console.log(e.description);
+        }
     }
-    catch (e) {
-        alert('Problem with server');
-        console.log(e.description);
-    }
-  }
 
     return parsingToJSON(dataSet);
 }
@@ -177,107 +172,236 @@ function parsingToJSON(text) {
 }
 
 //function that edits the URL parameters and returns edited url.
-function editUrlQuery(){
-  console.log(this.offenseType + " " + this.offenseYear + " " + this.offenseMonth + " " + this.offenseBorough + " " + " From editUrlQuery");
-      var url = 'https://data.cityofnewyork.us/resource/7x9x-zpz6.json?&ofns_desc=' + this.offenseType + '&boro_nm=' + this.offenseBorough;
-  return url;
+function editUrlQuery() {
+    var url;
+    if (this.offenseYear < 2016) {
+        console.log(this.offenseType + " " + this.offenseYear + " " + this.offenseMonth + " " + this.offenseBorough + " " + "Before 2016");
+
+        url = 'https://data.cityofnewyork.us/resource/9s4h-37hy.json?&ofns_desc=' + this.offenseType + '&boro_nm=' + this.offenseBorough;
+    } else {
+        url = 'https://data.cityofnewyork.us/resource/7x9x-zpz6.json?&ofns_desc=' + this.offenseType + '&boro_nm=' + this.offenseBorough;
+        console.log(this.offenseType + " " + this.offenseYear + " " + this.offenseMonth + " " + this.offenseBorough + " " + "On 2016");
+    }
+    return url;
 }
 
 //Object with all they keys required to edit url. Any search option user chooses gets assigned to appropriate key;
 var offenseSearchDetail = {
-  offenseType: null,
-  offenseMonth: null,
-  offenseBorough: null
+    offenseType: null,
+    offenseMonth: null,
+    offenseBorough: null,
+    offenseYear: null
 }
 
 //Gets the value in the select elements in html which contains all the search categories.
-function assignSearchSelection(){
-   this.offenseType = document.getElementById('offenseSelector').value;
-   this.offenseMonth = document.getElementById('monthSelector').value;
-   this.offenseBorough = document.getElementById('boroughSelector').value;
+function assignSearchSelection() {
+    this.offenseType = document.getElementById('offenseSelector').value;
+    this.offenseMonth = document.getElementById('monthSelector').value;
+    this.offenseBorough = document.getElementById('boroughSelector').value;
+    this.offenseYear = document.getElementById('yearSelector').value;
 
 
 }
 
 //event laaunched when search button is pressed. assignSearchSelection function assigns values to the keys in offenseSearchDetail object, which is then passed into
 //editUrlQuery which then inserts the value into the url and returns the url. startRequest makes an ajax call to the url.
-document.getElementById("searchButton").addEventListener('click', function() {
+$(document).ready(function() {
+    document.getElementById("searchButton").addEventListener('click', function() {
 
-    $(".navbar").addClass("slideUp"); //jquery add class that translates nav bar to top of page
-    assignSearchSelection.call(offenseSearchDetail);
-    var jsontext = startRequest(editUrlQuery.call(offenseSearchDetail));
-    initialize(jsontext);
+        $(".navbar").addClass("slideUp"); //jquery add class that translates nav bar to top of page
+        assignSearchSelection.call(offenseSearchDetail);
+        var jsontext = startRequest(editUrlQuery.call(offenseSearchDetail));
+        initialize(jsontext);
 
-}, false);
-
+    }, false);
+});
 //At them moment only using api that contains crimes for 2016
 //Create option values for the Select element. Option values contain years from 2000 - 2015
-/*function fillYearSelectElement(){
-  var yearSelection = document.getElementById("yearSelector");
-  var yearArray = [];
-  var total = 2000;
-  var selectOptions;
-  for (var i = 0; i<=15; i++){
-      yearArray[i] = total;
-      total++;
-  }
+function fillYearSelectElement() {
+    var yearSelection = document.getElementById("yearSelector");
+    var selectOptions;
 
-  //creat one option with each loop, give it a value and text (what the use sees). Then append the option to the Select element
-  for (let i = 0; i <= yearArray.length-1; i++){
-      selectOptions = document.createElement("option");
-      selectOptions.value = yearArray[i];
-      selectOptions.text = yearArray[i];
-      yearSelection.appendChild(selectOptions);
-  }
+    //Create option to show all data starting with year 2015 or older. using 2009 as a default value.
+    selectOptions = document.createElement("option");
+    selectOptions.value = 2009;
+    selectOptions.text = "2015 and older";
+    yearSelection.appendChild(selectOptions);
 
-}*/
+    //option to see 2016 data
+    selectOptions = document.createElement("option");
+    selectOptions.value = 2016;
+    selectOptions.text = "2016";
+    yearSelection.appendChild(selectOptions);
+}
 
 //Create option values for the Select element. Option values contain the months in the year
-function fillMonthSelectElement(){
-  var monthSelection = document.getElementById("monthSelector");
-  var monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October","November", "December"];
+function fillMonthSelectElement() {
+    var monthSelection = document.getElementById("monthSelector");
+    var monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-  //creat one option with each loop, give it a value and text (what the use sees). Then append the option to the Select element
-  for (let i = 0; i <= monthArray.length-1; i++){
-      selectOptions = document.createElement("option");
-      selectOptions.value = monthArray[i];
-      selectOptions.text = monthArray[i];
-      monthSelector.appendChild(selectOptions);
-  }
+    //creat one option with each loop, give it a value and text (what the use sees). Then append the option to the Select element
+    for (let i = 0; i <= monthArray.length - 1; i++) {
+        selectOptions = document.createElement("option");
+        selectOptions.value = monthArray[i];
+        selectOptions.text = monthArray[i];
+        monthSelector.appendChild(selectOptions);
+    }
 
 }
 
 //Create option values for the Select element. Option values contain the borough of new york
-function fillBoroughSelectElement(){
-  var boroughSelection = document.getElementById("boroughSelector");
-  var boroughArray = ["Brooklyn", "Queens", "Bronx", "Manhattan", "Staten Island"];
+function fillBoroughSelectElement() {
+    var boroughSelection = document.getElementById("boroughSelector");
+    var boroughArray = ["Brooklyn", "Queens", "Bronx", "Manhattan", "Staten Island"];
 
-  //creat one option with each loop, give it a value and text (what the use sees). Then append the option to the Select element
-  for (let i = 0; i <= boroughArray.length-1; i++){
-      selectOptions = document.createElement("option");
-      selectOptions.value = boroughArray[i].toUpperCase();
-      selectOptions.text = boroughArray[i];
-      boroughSelection.appendChild(selectOptions);
-  }
+    //creat one option with each loop, give it a value and text (what the use sees). Then append the option to the Select element
+    for (let i = 0; i <= boroughArray.length - 1; i++) {
+        selectOptions = document.createElement("option");
+        selectOptions.value = boroughArray[i].toUpperCase();
+        selectOptions.text = boroughArray[i];
+        boroughSelection.appendChild(selectOptions);
+    }
 }
 
 //Create option values for the Select element. Option values contain offense types
-function fillOffenseTypeSelectElement(){
-  var offenseSelection = document.getElementById("offenseSelector");
-  var offenseArray = ["Burglary", "Felony Assault", "Grand Larceny", "Grand Larceny of Motor Vehicle", "Murder & Non-negl. Manslaughte", "Rape", "Robbery"];
+function fillOffenseTypeSelectElement() {
+    var offenseSelection = document.getElementById("offenseSelector");
+    var offenseArray = ["Arson", "Assault 3 & Related Offenses", "Burglary", "Child Abandonment/Non Support", "Felony Assault", "Grand Larceny", "Grand Larceny of Motor Vehicle", "Homicide-Negligent-Vehicle", "Kidnapping", "Rape", "Robbery", "Theft-Fraud"];
 
-  //creat one option with each loop, give it a value and text (what the use sees). Then append the option to the Select element
-  for (let i = 0; i <= offenseArray.length-1; i++){
-      selectOptions = document.createElement("option");
-      selectOptions.value = offenseArray[i].toUpperCase();
-      selectOptions.text = offenseArray[i];
-      offenseSelection.appendChild(selectOptions);
-  }
+    //creat one option with each loop, give it a value and text (what the use sees). Then append the option to the Select element
+    for (let i = 0; i <= offenseArray.length - 1; i++) {
+        selectOptions = document.createElement("option");
+        selectOptions.value = offenseArray[i].toUpperCase();
+        selectOptions.text = offenseArray[i];
+        offenseSelection.appendChild(selectOptions);
+    }
+}
+
+function mapStyle(){
+  //black and white style for google maps https://snazzymaps.com/style/8097/wy
+  var styledMapType = new google.maps.StyledMapType(
+      [{
+          "featureType": "all",
+          "elementType": "geometry.fill",
+          "stylers": [{
+              "weight": "2.00"
+          }]
+      }, {
+          "featureType": "all",
+          "elementType": "geometry.stroke",
+          "stylers": [{
+              "color": "#9c9c9c"
+          }]
+      }, {
+          "featureType": "all",
+          "elementType": "labels.text",
+          "stylers": [{
+              "visibility": "on"
+          }]
+      }, {
+          "featureType": "landscape",
+          "elementType": "all",
+          "stylers": [{
+              "color": "#f2f2f2"
+          }]
+      }, {
+          "featureType": "landscape",
+          "elementType": "geometry.fill",
+          "stylers": [{
+              "color": "#ffffff"
+          }]
+      }, {
+          "featureType": "landscape.man_made",
+          "elementType": "geometry.fill",
+          "stylers": [{
+              "color": "#ffffff"
+          }]
+      }, {
+          "featureType": "poi",
+          "elementType": "all",
+          "stylers": [{
+              "visibility": "off"
+          }]
+      }, {
+          "featureType": "road",
+          "elementType": "all",
+          "stylers": [{
+              "saturation": -100
+          }, {
+              "lightness": 45
+          }]
+      }, {
+          "featureType": "road",
+          "elementType": "geometry.fill",
+          "stylers": [{
+              "color": "#eeeeee"
+          }]
+      }, {
+          "featureType": "road",
+          "elementType": "labels.text.fill",
+          "stylers": [{
+              "color": "#7b7b7b"
+          }]
+      }, {
+          "featureType": "road",
+          "elementType": "labels.text.stroke",
+          "stylers": [{
+              "color": "#ffffff"
+          }]
+      }, {
+          "featureType": "road.highway",
+          "elementType": "all",
+          "stylers": [{
+              "visibility": "simplified"
+          }]
+      }, {
+          "featureType": "road.arterial",
+          "elementType": "labels.icon",
+          "stylers": [{
+              "visibility": "off"
+          }]
+      }, {
+          "featureType": "transit",
+          "elementType": "all",
+          "stylers": [{
+              "visibility": "off"
+          }]
+      }, {
+          "featureType": "water",
+          "elementType": "all",
+          "stylers": [{
+              "color": "#46bcec"
+          }, {
+              "visibility": "on"
+          }]
+      }, {
+          "featureType": "water",
+          "elementType": "geometry.fill",
+          "stylers": [{
+              "color": "#c8d7d4"
+          }]
+      }, {
+          "featureType": "water",
+          "elementType": "labels.text.fill",
+          "stylers": [{
+              "color": "#070707"
+          }]
+      }, {
+          "featureType": "water",
+          "elementType": "labels.text.stroke",
+          "stylers": [{
+              "color": "#ffffff"
+          }]
+      }], {
+          name: 'Styled Map'
+      });
+
+      return styledMapType;
 }
 
 
-window.onload = function(){
-    //fillYearSelectElement();
+window.onload = function() {
+    fillYearSelectElement();
     fillMonthSelectElement();
     fillBoroughSelectElement();
     fillOffenseTypeSelectElement();
